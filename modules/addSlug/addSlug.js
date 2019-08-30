31f996887f71332cbdd4aed3283b85f2a073f8fd
Matthieu Jacques <matthieu.jacques.dev@gmail.com>
2019-08-30 10:48:38 +0200

    fix

---
+++ modules/addSlug/addSlug.js
@@ -0,0 +1,29 @@
+const db = require('../../dist/establishement.json');
+const fs = require('fs');
+
+function slugify(string) {
+    const a = 'àáâäæãåāăąçćčđďèéêëēėęěğǵḧîïíīįìłḿñńǹňôöòóœøōõṕŕřßśšşșťțûüùúūǘůűųẃẍÿýžźż·/_,:;'
+    const b = 'aaaaaaaaaacccddeeeeeeeegghiiiiiilmnnnnooooooooprrsssssttuuuuuuuuuwxyyzzz------'
+    const p = new RegExp(a.split('').join('|'), 'g')
+  
+    return string.toString().toLowerCase()
+      .replace(/\s+/g, '-') // Replace spaces with -
+      .replace(p, c => b.charAt(a.indexOf(c))) // Replace special characters
+      .replace(/&/g, '-and-') // Replace & with 'and'
+      .replace(/[^\w\-]+/g, '') // Remove all non-word characters
+      .replace(/\-\-+/g, '-') // Replace multiple - with single -
+      .replace(/^-+/, '') // Trim - from start of text
+      .replace(/-+$/, '') // Trim - from end of text
+  }
+
+for(var i = 0; i < db.length; i++) {
+    let newdb = slugify(db[i].name);
+    db[i]['slug'] = newdb;
+}
+console.log(db);
+
+const toto = db; 
+
+fs.writeFile('../../dist/database.json',  JSON.stringify(toto), function(err, result) {
+    if(err) console.log('error', err);
+});
