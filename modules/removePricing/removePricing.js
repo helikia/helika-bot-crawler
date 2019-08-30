d49e92d810815f19e869ff5ba329fbef0116e25c
Matthieu Jacques <matthieu.jacques.dev@gmail.com>
2019-08-30 10:46:17 +0200

    fix

--- modules/removePricing/removePricing.js
+++ modules/removePricing/removePricing.js
@@ -1,16 +1,30 @@
-const data = require('../../dist/db/db.json');
-const fs = require('fs');
+const mongoose = require('mongoose');
+const Fixtures = require('node-mongodb-fixtures');
 
-for ( var i in data ) {
-    for ( var k in data[i] ) {
-        // console.log(data[i][k].categories, '....................................................'); 
-        data[i][k].pricing = null;
-        data[i][k].categories = null;
-    }
-}
-
-let dbJson = JSON.stringify(data);
-
-fs.writeFile('../../dist/db/db.json', dbJson, function(err, result) {
-    if(err) console.log('error', err);
+// CONNECT TO DATABASE
+const fixtures = new Fixtures({
+    dir: './fixtures/etablishement'
 });
+
+const Database = 'mongodb://localhost:27017/helikia-app';
+mongoose.connect(Database, { 
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+fixtures.connect(Database)
+    .then(() => fixtures.unload())
+    .then(() => fixtures.load())
+    .catch(console.error)
+    .finally(() => fixtures.disconnect());
+
+// for ( var i in data ) {
+//     for ( var k in data[i] ) {
+//         console.log(data[i][k]);
+//     }
+// }
+
+// let dbJson = JSON.stringify(data);
+
+// fs.writeFile('../../dist/db/db.json', dbJson, function(err, result) {
+//     if(err) console.log('error', err);
+// });
